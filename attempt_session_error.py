097053fabd0487a914123300cf0f5e6674257675
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-11-25 00:06:22 -0600

    Create attempt_session_error.py

---
+++ attempt_session_error.py
@@ -0,0 +1,24 @@
+import time
+
+from river_mwclient.gamepedia_client import GamepediaClient
+from river_mwclient.auth_credentials import AuthCredentials
+
+credentials = AuthCredentials(user_file="me")
+site = GamepediaClient('wikisandbox-ucp', credentials=credentials)
+summary = 'Bot edit'
+
+# Attempt to encounter login errors and count approximately how frequently they occur
+# The goal was just to obtain some data
+# After running this I ended up adding a bunch of error-checking/retrying code to river_mwclient
+
+for trial in range(0, 10):
+    i = 0
+    while True:
+        try:
+            site.client.pages['User:RheingoldRiver/counter'].save(str(i))
+            time.sleep(60)
+            i += 1
+        except Exception as e:
+            site.login()
+            break
+    site.client.pages['User:RheingoldRiver/counter report'].append('\n' + str(i))
