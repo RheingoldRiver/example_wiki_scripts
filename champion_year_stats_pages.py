abec2060c255a61f95ad0c04ebae9a495187e82d
River <18037011+RheingoldRiver@users.noreply.github.com>
2021-03-31 14:24:14 -0500

    Update champion_year_stats_pages.py

--- champion_year_stats_pages.py
+++ champion_year_stats_pages.py
@@ -3,9 +3,11 @@ from river_mwclient.auth_credentials import AuthCredentials
 
 credentials = AuthCredentials(user_file="me")
 site = EsportsClient('lol', credentials=credentials)
-summary = 'Creating year stats pages for 2021'
+new_year = '2021'
+summary = 'Creating year stats pages for {}'.format(new_year)
 
 
 for champion_row in site.cargo_client.query(tables="Champions", fields="Name", limit="max"):
 	champion = champion_row['Name']
-	site.save_title(champion + '/Statistics/2021', '{{ChampionYearStatsPage}}', summary=summary)
+	site.save_title(champion + '/Statistics/{}'.format(new_year), '{{ChampionYearStatsPage}}', summary=summary)
+	site.save_title(champion + '/Statistics', '#redirect [[{}/Statistics/{}]]'.format(champion, new_year))
